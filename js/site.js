f4daa9ad40f129f5869e95eb7c0173f428633475
thubmeoc <47995576+thubmeoc@users.noreply.github.com>
2019-03-26 21:00:06 -0400

    add javascript

---
+++ js/site.js
@@ -0,0 +1,22 @@
+console.log("External file");
+
+var city;
+var cityFrom = "";
+var cityName = "New York City";
+var x = 32;
+
+console.log("cityName, x: ", cityName, x);
+
+var firstname = "Tedra", lastname = "Moneee";
+console.log("firstname, lastname", firstname, lastname);
+
+var s = "2019";
+var s2 = 2019
+var n =  2001;
+console.log("age: ", s - n);
+console.log("age:", s + n);
+console.log("this year: ", s + "born year:" + n);
+console.log("age: ", s2 + n);
+
+ var escapequotes = "This is T's\"city\" name";
+console.log ("escapequotes: ", escapequotes);
